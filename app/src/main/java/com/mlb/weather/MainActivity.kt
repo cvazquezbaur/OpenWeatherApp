b47d64edec8c2cfa37e0afe54eb9272af9053cc9
carlos.baur <carlos.baur@fdmgroup.com>
2024-11-27 13:22:30 -0700

    Making Progress, but working really intermittently. Will do more Friday

--- app/src/main/java/com/mlb/weather/MainActivity.kt
+++ app/src/main/java/com/mlb/weather/MainActivity.kt
@@ -6,42 +6,53 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.compose.rememberNavController
+import com.mlb.weather.presentation.WeatherLandingFragment
 import com.mlb.weather.ui.theme.MLB_WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MLB_WeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppLaunch()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppLaunch() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Weather Report") } )
+         },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Weather Report",
+            modifier = Modifier.padding(it)
+        )
+//        NavigationGraph(navController = navController, modifier = Modifier.padding(it))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MLB_WeatherAppTheme {
-        Greeting("Android")
+//        Greeting("Android")
     }
 }
--- app/src/main/java/com/mlb/weather/presentation/WeatherLandingFragment.kt
+++ app/src/main/java/com/mlb/weather/presentation/WeatherLandingFragment.kt
@@ -11,15 +11,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.Modifier
 import com.mlb.weather.ui.theme.MLB_WeatherAppTheme
 
 class WeatherLandingFragment : Fragment() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,7 +32,9 @@ class WeatherLandingFragment : Fragment() {
             setContent {
                 MLB_WeatherAppTheme {
                     Scaffold(
-                        topBar = { TopBar() },
+                        topBar = { TopAppBar(
+                            title = { Text("Weather Report") }
+                        ) },
                         modifier = Modifier.fillMaxSize()
                     ) { innerPadding ->
                         WeatherList(Modifier.padding(innerPadding))
